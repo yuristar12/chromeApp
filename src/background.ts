3b18c1f9e0a9c577cf99b0dd6b4660c25b9838bf
yuri_star <yuri19951212@outlook.com>
2024-07-10 20:42:35 +0800

    提交代码

--- src/background.ts
+++ src/background.ts
@@ -8,30 +8,21 @@ browser.runtime.onInstalled.addListener((details) => {
 
 
 chrome.runtime.onMessage.addListener(async (message: string) => {
-
-    console.log(message);
-
     if (message === 'onPopupMounted') {
 
-        const id = await GetTabsIdByUrl('https://developer.chrome.google.cn/docs/extensions?hl=zh-cn');
-        console.log(id);
-
-        if (id) {
-            chrome.scripting.executeScript({
-                target: {tabId: id},
-                func: test
-            })
-        }
+        // const id = await GetTabsIdByUrl('https://developer.chrome.google.cn/docs/extensions?hl=zh-cn');
+        // console.log(id);
+        //
+        // if (id) {
+        //     chrome.scripting.executeScript({
+        //         target: {tabId: id},
+        //         func: test
+        //     })
+        // }
     }
     return true;
-
 });
 
-function test() {
-    const div = document.querySelector("body > section > devsite-header > div > div.devsite-top-logo-row-wrapper-wrapper > div > div > devsite-appearance-selector").shadowRoot.querySelector("button");
-    if (div) {
-        div.click();
-    }
-}
+
 
 
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,12 +1,10 @@
 export const GetTabsIdByUrl = async (url: string) => {
     let id = null;
-
     const tabsList = await chrome.tabs.query({});
     // console.log(tabsList);
     if (Array.isArray(tabsList)) {
         for (let i = 0; i < tabsList.length; i++) {
             const item = tabsList[i];
-            console.log(item);
             if (item.url!.indexOf(url) > -1) {
                 id = item.id;
                 break;
