1d4a14e7c7eaa0a69ae44be2257085ec2f7e344e
yuri_star <yuri19951212@outlook.com>
2024-07-10 20:13:33 +0800

    入库

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+import * as path from "node:path";
+
+function generateManifest() {
+  const manifest = readJsonFile("src/manifest.json");
+  const pkg = readJsonFile("package.json");
+  return {
+    name: pkg.name,
+    description: pkg.description,
+    version: pkg.version,
+    ...manifest,
+  };
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    webExtension({
+      manifest: generateManifest,
+      watchFilePaths: ["package.json", "manifest.json"],
+    }),
+  ],
+});
